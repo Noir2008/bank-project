8d5079857e5008ea0903937a9fe9643cf6dbff92
Комп-9 Моноблок Ка39 <kostanemiro23@gmail.com>
2026-03-25 09:50:28 +1000

    "Обновил widget"

--- src/masks.py
+++ src/masks.py
@@ -1,14 +1,14 @@
 def get_mask_card_number(card_number):
-    print(f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[12:16]}")
+    print(f"Номер карты: {card_number[0:4]} {card_number[4:6]}** **** {card_number[12:16]}")
 
 def get_mask_account(mask_account):
-    print(f"**{mask_account[-4:]}")
+    print(f"Номер счета: **{mask_account[-4:]}")
 
-# 1234567890123456
-input_card_number = input("Введите номер карты: ")
-
-# 12345678901234567890
-input_mask_account = input("Введите номер счета: ")
-
-get_mask_card_number(input_card_number)
-get_mask_account(input_mask_account)
+# # 1234567890123456
+# input_card_number = input("Введите номер карты: ")
+#
+# # 12345678901234567890
+# input_mask_account = input("Введите номер счета: ")
+#
+# get_mask_card_number(input_card_number)
+# get_mask_account(input_mask_account)
---
+++ src/widget.py
@@ -0,0 +1,33 @@
+from masks import get_mask_account, get_mask_card_number
+
+def mask_account_card(name_card):
+    num = "1234567890"
+    i_test = 0
+    for i in name_card:
+        if i in num:
+            print(f"{name_card[0:i_test-1]} {name_card[i_test:i_test+4]} {name_card[i_test+4:i_test+6]}** **** {name_card[-4:]}")
+            break
+        i_test += 1
+
+# 1234567890123456
+input_card_number = input("Введите номер карты: ")
+
+# 12345678901234567890
+input_mask_account = input("Введите номер счета: ")
+
+get_mask_card_number(input_card_number)
+get_mask_account(input_mask_account)
+
+# Visa Platinum 7000792289606361
+# Maestro 7000792289606361
+# Maestro 1596837868705199
+# Счет 64686473678894779589
+# MasterCard 7158300734726758
+# Счет 35383033474447895560
+# Visa Classic 6831982476737658
+# Visa Platinum 8990922113665229
+# Visa Gold 5999414228426353
+# MasterCard 7158300734726758
+# Счет 73654108430135874305
+name_card_input = input("Введите название и номер карты: ")
+mask_account_card(name_card_input)
