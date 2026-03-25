a1e439968c941f8a51c2385f55eabe2b0f1c1d79
Комп-9 Моноблок Ка39 <kostanemiro23@gmail.com>
2026-03-25 10:03:31 +1000

    Отформатировал код

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,12 @@
 def get_mask_card_number(card_number):
-    print(f"Номер карты: {card_number[0:4]} {card_number[4:6]}** **** {card_number[12:16]}")
+    print(
+        f"Номер карты: {card_number[0:4]} {card_number[4:6]}** **** {card_number[12:16]}"
+    )
+
 
 def get_mask_account(mask_account):
     print(f"Номер счета: **{mask_account[-4:]}")
+
 
 # # 1234567890123456
 # input_card_number = input("Введите номер карты: ")
--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,17 @@
 from masks import get_mask_account, get_mask_card_number
 
+
 def mask_account_card(name_card):
     num = "1234567890"
     i_test = 0
     for i in name_card:
         if i in num:
-            print(f"{name_card[0:i_test-1]} {name_card[i_test:i_test+4]} {name_card[i_test+4:i_test+6]}** **** {name_card[-4:]}")
+            print(
+                f"{name_card[0:i_test-1]} {name_card[i_test:i_test+4]} {name_card[i_test+4:i_test+6]}** **** {name_card[-4:]}"
+            )
             break
         i_test += 1
+
 
 # 1234567890123456
 input_card_number = input("Введите номер карты: ")
